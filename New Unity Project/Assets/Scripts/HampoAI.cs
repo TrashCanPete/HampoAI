dbe8e49d8dbfe23dca9f67e9a274de6993d32229
Peter McDermott <mcdermpete@gmail.com>
2019-09-09 11:47:47 +1200

    Little Hambo

---
+++ New Unity Project/Assets/Scripts/HampoAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HampoAI : MonoBehaviour
+{
+    public List<Transform> target;
+    private NavMeshAgent agent;
+    private int currentDestination;
+    public float targetDistanceThreshold;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < targetDistanceThreshold)
+        {
+            SetNewDestination();
+        }
+
+    }
+
+    private void SetNewDestination()
+    {
+        currentDestination++;
+
+        if (currentDestination > target.Count - 1)
+        {
+            currentDestination = Mathf.RoundToInt(Random.Range(0f, 3f));
+        }
+            agent.SetDestination(target[currentDestination].position);
+        }
+}

cfe4aac8768a82a762d013cbe048962162ac01d4
Peter McDermott <mcdermpete@gmail.com>
2019-09-11 11:52:34 +1200

    FPS HAMPO

---
+++ New Unity Project/Assets/Scripts/HitscanPistol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitscanPistol : MonoBehaviour
+{
+    public Transform gunBarrel;
+    public GameObject explosion;
+    public GameObject blood;
+    
+    
+
+    void Update()
+    {
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(gunBarrel.position, gunBarrel.forward, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(gunBarrel.position, gunBarrel.forward * hit.distance, Color.yellow);
+                GameObject explosionClone = Instantiate(explosion);
+                explosion.transform.position = hit.point;
+                GameObject bloodClone = Instantiate(blood);
+                blood.transform.position = hit.point;
+                if (hit.collider.transform.tag == "enemy")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(gunBarrel.position, gunBarrel.forward * 9000, Color.grey);
+            }
+
+            
+        }
+
+        
+       
+
+    }
+}
